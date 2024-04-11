65a334cce0c12ce3c70bd52837a85cc6fa622797
dylanyang <yl976931569@163.com>
2024-04-11 14:12:20 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,64 @@ fn main() {
     // }
     // let _x = 5;
     // let y = 1;
-    test_shadowing()
+    test_char()
+}
+fn test_type(){
+    let x = 2.0;
+    let y :f32 = 3.0;
+}
+fn test_NaN(){
+    let x = (-42.0_f32).sqrt();
+    if x.is_nan() {
+        println!("undefined")
+    }
+}
+fn test_number(){
+    let v: u16 = 38_u8 as u16;
+    let arr = [
+        42.0,
+        42f32,
+        42.0_f32
+    ];
+    let x = "22323";
+    println!("size x [{}]",std::mem::size_of_val(&x));
+    println!("{:.10}",arr[0]);
+    let y = 5;
+    assert_eq!("i32".to_string(), type_of(&x));
+
+
+    assert!(0.1_f32+0.2_f32==0.3_f32);
+}
+use std::mem::size_of_val;
+fn test_char(){
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),4);
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),4);
+
+    let v0: () = ();
+
+    let v = (2, 3);
+    assert_eq!(v, implicitly_ret_unit());
+
+    println!("Success!")
+}
+
+fn implicitly_ret_unit() {
+    println!("I will return a ()")
+}
+fn print_char(c : char) {
+    println!("{}", c);
+}
+
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
+fn test_range(){
+    for i in 0..=5{
+        print!("{}",i);
+    }
 }
 
 fn test_var() {
